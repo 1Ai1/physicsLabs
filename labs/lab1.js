1c6e5ca8543b2d33934470e92554887204d3cc8d
1Ai1 <azamatov.11.08@gmail.com>
2024-04-10 22:57:34 +0500

    correcteb lab page

--- labs/lab1.js
+++ labs/lab1.js
@@ -30,43 +30,23 @@ var runner = Runner.create();
 Runner.run(runner, engine);
 
 // add bodies
-var stack = Composites.stack(
-  0,
-  500 - 21 - 20 * 20,
-  5,
-  3,
-  20,
-  0,
-  function (x, y) {
-    return Bodies.circle(x, y, 20, {
-      elem: document.querySelector("#box"),
-      render: {
-        fillStyle: "#FFD700",
-        strokeStyle: "black",
-        lineWidth: 1,
-        // sprite: {
-        //   texture: "../assets/Red-Ball-PNG-Clipart.png",
-        //   xScale: 0.06,
-        //   yScale: 0.06,
-        // },
-      },
-    });
-  }
-);
 
-var door = Bodies.rectangle(700, 20, 100, 100);
+var door = Bodies.rectangle(700, 0, 150, 100);
 
 Composite.add(world, [
   // walls
-  Bodies.rectangle(150, 220, 300, 30, { isStatic: true }),
-  Bodies.rectangle(300, 185, 30, 100, { isStatic: true }),
   Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
   Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
   Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
-  stack,
   door,
 ]);
+
+function addBall() {
+  ball = Bodies.circle(Math.random() * 100, 300, 20);
+  Composite.add(world, ball);
+}
+
 // add mouse control
 var mouse = Mouse.create(render.canvas),
   mouseConstraint = MouseConstraint.create(engine, {
